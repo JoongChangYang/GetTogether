57a3a4446820ac87009e02dc3e4313e7171b2bd4
Hailey <foxwavez@gmail.com>
2020-02-03 19:58:54 +0900

    테스트 커밋

---
+++ GetTogether/GetTogether/Controller/GetTgetherListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GetTgetherListViewController.swift
+//  GetTogether
+//
+//  Created by Hailey Lee on 2020/02/03.
+//  Copyright © 2020 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class GetTgetherListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
